e3f1bd0155fb42c0e138c1fbdb9a52d7383878c1
graciaSSS4 <72733082+graciaSSS4@users.noreply.github.com>
2026-07-06 18:25:21 +0200

    remodif pour debugg

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,7 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "pages/home.html"),
+    new Route("/", "Accueil", "/pages/home.html"),
     
 ];
 
