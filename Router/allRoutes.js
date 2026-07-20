d0e087462bae569b9c07364a4a4e689a76d8daae
graciaSSS4 <graciaSSS4@hotmail.fr>
2026-07-20 19:22:32 +0200

    ajout des pages account + créa files auth

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,9 +5,10 @@ export const allRoutes = [
     new Route("/", "Accueil", "pages/home.html"),
     new Route("/galerie", "La Galerie", "pages/galerie.html"),
     new Route("/carte", "La Carte", "pages/carte.html"),
-    new Route("/signin", "Connexion", "pages/signin.html"),
-    new Route("/signup", "Inscription", "pages/signup.html"),
-
+    new Route("/signin", "Connexion", "pages/auth/signin.html"),
+    new Route("/signup", "Inscription", "pages/auth/signup.html"),
+    new Route("/account", "Mon compte", "pages/auth/account.html"),
+    new Route("/editPassword", "Changement de mot de passe", "pages/auth/editPassword.html"),
 ];
     
 
