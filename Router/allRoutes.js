31e60801a6e52877040d040905987e10ebac2a97
graciaSSS4 <graciaSSS4@hotmail.fr>
2026-07-20 20:17:25 +0200

    test apparemment changement ds main.css.map + ajout page réservations

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -9,6 +9,7 @@ export const allRoutes = [
     new Route("/signup", "Inscription", "pages/auth/signup.html"),
     new Route("/account", "Mon compte", "pages/auth/account.html"),
     new Route("/editPassword", "Changement de mot de passe", "pages/auth/editPassword.html"),
+    new Route("/reservation", "Vos réservations", "pages/reservation.html"),
 ];
     
 
