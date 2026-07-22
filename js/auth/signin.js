9978f7f6ae2fe45d68f0d9ff0b84895fa88f077d
graciaSSS4 <graciaSSS4@hotmail.fr>
2026-07-22 19:39:53 +0200

    ajout page signin.js + header page carte

---
+++ js/auth/signin.js
@@ -0,0 +1,29 @@
+//Implémenter une javascript de ma page
+
+const inputEmail = document.getElementById("EmailInput");
+const inputPassword = document.getElementById("PasswordInput");
+const btnSingin = document.getElementById("btnSingin");
+
+btnSingin.addEventListener("click", checkCredentials);
+
+function checkCredentials() {
+    //Ici, il faudra appeler l'API pour vérifier les credentials en BDD
+    if (EmailInput.value == "test@gmail.com" && PasswordInput.value == "123") {
+        alert("Vous êtes bien connecté !");
+
+        //Il faudra récupérer le vrai token
+        const token = "lkjsdngfljsqdnglkjsdbglkjqskjgkfjgbqslkfdgbskldfgdfgsdgf";
+        setToken(token);
+        
+        //placer ce token en cookie
+        setCookie(RoleCookieName, "admin", 7);
+
+        window.location.replace("/");
+
+    } else {
+        EmailInput.classList.add("is-invalid");
+        PasswordInput.classList.add("is-invalid");  // red
+
+    }
+}
+
