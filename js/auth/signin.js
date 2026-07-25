816d556d604185ebdf18bed1bb42288f69de049f
graciaSSS4 <graciaSSS4@hotmail.fr>
2026-07-25 23:35:26 +0200

    ajout du bouton déconnexion dans script.js

--- js/auth/signin.js
+++ js/auth/signin.js
@@ -4,20 +4,20 @@ const inputEmail = document.getElementById("EmailInput");
 const inputPassword = document.getElementById("PasswordInput");
 const btnSingin = document.getElementById("btnSingin");
 
+
 btnSingin.addEventListener("click", checkCredentials);
+
 
 function checkCredentials() {
     //Ici, il faudra appeler l'API pour vérifier les credentials en BDD
     if (EmailInput.value == "test@gmail.com" && PasswordInput.value == "123") {
-        alert("Vous êtes bien connecté !");
-
+       
         //Il faudra récupérer le vrai token
         const token = "lkjsdngfljsqdnglkjsdbglkjqskjgkfjgbqslkfdgbskldfgdfgsdgf";
         setToken(token);
         
         //placer ce token en cookie
         
-
         window.location.replace("/");
 
     } else {
