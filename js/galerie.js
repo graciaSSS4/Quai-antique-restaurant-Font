b66c8493256c03ab92c2cebbf406d1df7a665620
graciaSSS4 <graciaSSS4@hotmail.fr>
2026-09-04 16:44:19 +0200

    ajout d'éviter de failles d'injection html gérer par le user part 2

--- js/galerie.js
+++ js/galerie.js
@@ -1,1 +1,27 @@
-function getImage()
+const galerieImage = document.getElementById("allImages");
+let monImage = getImage("titre", "../Image/IMAGE_quai-antique-images_2026-07-18_1108/fresh vegetables.jpg");
+
+// Récupérer les informations des images
+let titre;
+let imgSources;
+
+galerieImage.innerHTML = monImage;
+
+function getImage(titre, urlImage) {
+  titre = sanitizeHtml(titre);
+  urlImage = sanitizeHtml(urlImage);
+  return ` <div class="col p-3">
+      <div class="image-card text-white">
+        <img src="${urlImage}" class="rounded w-100" />
+        <p class="titre-image">${titre}</p>
+        <div class="action-image-buttons" data-show="admin">
+          <button type="button" class="btn btn-outline-light" data-bs-toggle="modal" data-bs-target="#EditionPhotoModal">
+            <i class="bi bi-pencil-square"></i>
+          </button>
+          <button type="button" class="btn btn-outline-light" data-bs-toggle="modal" data-bs-target="#DeletePhotoModal">
+            <i class="bi bi-trash"></i>
+          </button>
+        </div>
+      </div>
+    </div> `;
+}
