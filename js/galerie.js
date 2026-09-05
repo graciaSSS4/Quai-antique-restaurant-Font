66156d67af9fb841bf60a22bd82cc61f43e6d722
graciaSSS4 <graciaSSS4@hotmail.fr>
2026-09-05 19:31:23 +0200

    ajout d'éviter de failles d'injection html gérer par le user part 2

--- js/galerie.js
+++ js/galerie.js
@@ -7,6 +7,18 @@ let imgSources;
 
 galerieImage.innerHTML = monImage;
 
+function sanitizeHtml(text) {
+  // Créez un élément HTML temporaire de type "div"
+  const tempHtml = document.createElement("div");
+
+  // Affectez le texte reçu en tant que contenu texte brut de l'élément "tempHtml"
+  tempHtml.textContent = text;
+
+  // Utilisez .innerHTML pour récupérer le contenu de "tempHtml"
+  // Cela va "neutraliser" ou "échapper" tout code HTML potentiellement malveillant
+  return tempHtml.innerHTML;
+}
+
 function getImage(titre, urlImage) {
   titre = sanitizeHtml(titre);
   urlImage = sanitizeHtml(urlImage);
--- js/script.js
+++ js/script.js
@@ -87,3 +87,15 @@ function showAndHideElementsForRoles() {
     }
   });
 }
+
+function sanitizeHtml(text) {
+  // Créez un élément HTML temporaire de type "div"
+  const tempHtml = document.createElement("div");
+
+  // Affectez le texte reçu en tant que contenu texte brut de l'élément "tempHtml"
+  tempHtml.textContent = text;
+
+  // Utilisez .innerHTML pour récupérer le contenu de "tempHtml"
+  // Cela va "neutraliser" ou "échapper" tout code HTML potentiellement malveillant
+  return tempHtml.innerHTML;
+}
